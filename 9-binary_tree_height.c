ffc02c82ba1117c71df04bc133d16b51898f37d6
trOyYeyY <troyzuwara@gmail.com>
2024-02-29 23:02:08 +0000

    fixing hieght

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,9 +12,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	if (tree- left_ht)
-		left_ht += 1 + binary_tree_height(tree- left_ht);
-	if (tree->right_ht)
-		right_ht += 1 + binary_tree_height(tree->right_ht);
+	if (tree->left)
+		left_ht += 1 + binary_tree_height(tree->left);
+	if (tree->right)
+		right_ht += 1 + binary_tree_height(tree->right);
 	return  (left_ht > right_ht ? left_ht : right_ht);
 }
